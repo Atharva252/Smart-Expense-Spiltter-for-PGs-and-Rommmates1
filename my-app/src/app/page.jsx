72317ff23ad8648bee70a315ae3f01767f002841
CrazZy000 <siddeshpingle57995@gmail.com>
2025-09-05 17:09:56 +0530

    Glassdashbaord

--- my-app/src/app/page.jsx
+++ my-app/src/app/page.jsx
@@ -1,10 +1,13 @@
 import Footer from "@/components/common/Footer";
 import Navbar from "@/components/common/Navbar";
+import GlassDashboard from "@/components/GlassDashboard/GlassDashboard";
 
 export default function Home() {
   return (
    <>
      <Navbar />
+     
+     <GlassDashboard />
      <Footer />
    </>
   );
